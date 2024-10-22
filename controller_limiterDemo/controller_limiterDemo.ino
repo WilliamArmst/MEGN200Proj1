61ca10212539d781c6f4030929758230d72a14d5
William <william_armstrong@mines.edu>
2024-10-22 12:21:16 -0600

    clamp and arm limiter demo code

--- controller_limiterDemo/controller_limiterDemo.ino
+++ controller_limiterDemo/controller_limiterDemo.ino
@@ -1,9 +1,56 @@
-void setup() {
-  // put your setup code here, to run once:
+// #include <SPI.h>
+// #include <nRF24L01.h>
+// #include <RF24.h>
+#include <Servo.h>
 
+// servos
+#define ARM_SERVO A0
+#define CLAMP_SERVO A1
+
+Servo armServo;
+Servo clampServo;
+
+// 0 to 180
+int armPos = 90;
+int clampPos = 90;
+
+const int minArmPos = 70; // arm is theoretically 50-150
+const int maxArmPos = 130;
+const int minClampPos = 70; // clamp is theoretically 50-140
+const int maxClampPos = 120;
+
+void setup() {
+  Serial.begin(9600);
+  
+  // set pins to output
+  pinMode(ARM_SERVO, OUTPUT);
+  pinMode(CLAMP_SERVO, OUTPUT);
+
+  armServo.attach(ARM_SERVO);
+  clampServo.attach(CLAMP_SERVO);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // armPos += 1; // test max arm
+  // armPos -= 1; // test min arm
+  if (armPos > maxArmPos) {
+    armPos = maxArmPos;
+  }
+  if (armPos < minArmPos) {
+    armPos = minArmPos;
+  }
+  armServo.write(armPos);
+
+  // clampPos += 1; // test max clamp
+  // clampPos -= 1; // test min clamp
+  if (clampPos > maxClampPos) {
+    clampPos = maxClampPos;
+  }
+  if (clampPos < minArmPos) {
+    clampPos = minClampPos;
+  }
+  clampServo.write(clampPos);
+
+  delay(3);
 
 }
